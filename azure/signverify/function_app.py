ceb42aba38bb54ebb4437a342e68a79d64db00cb
Aleksandr Tserepov-Savolainen <aleksandr.tserepov-savolainen@unikie.com>
2024-10-08 10:49:42 +0300

    Initial commit of SignVerify AzureFunction
    TODO:
       - pylint
       - code cleanup
    
    Signed-off-by: Aleksandr Tserepov-Savolainen <aleksandr.tserepov-savolainen@unikie.com>

---
+++ azure/signverify/function_app.py
@@ -0,0 +1,56 @@
+import logging
+import azure.functions as func
+from azure.identity import DefaultAzureCredential
+from azure.keyvault.keys import KeyClient
+from azure.keyvault.keys.crypto import CryptographyClient, SignatureAlgorithm
+
+import json
+import base64
+
+app = func.FunctionApp()
+
+key_vault_url = "https://ghaf-devenv-ca.vault.azure.net/"
+#credential = DefaultAzureCredential()
+#key_client = KeyClient(vault_url=key_vault_url, credential=credential)
+
+@app.route(route="VerifySignature", auth_level=func.AuthLevel.ANONYMOUS)
+def VerifySignature(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('Python HTTP trigger function processed a request.')
+
+    try:
+        req_body = req.get_json()
+        if not all(field in req_body for field in ['certificateName', 'Signature', 'Hash']):
+            return func.HttpResponse(
+                body=json.dumps({'error': 'Missing required fields', 'message': 'Expected certificateName, Signature, and Hash'}),
+                mimetype="application/json",
+                status_code=400
+            )
+
+        certificate_name = req_body['certificateName']
+        b64signature = req_body['Signature']
+        b64hash_value = req_body['Hash']
+        logging.info ("Req data: b64Signature: "+b64signature+" b64Hash: "+b64hash_value)
+
+        key = key_client.get_key(certificate_name)
+        crypto_client = CryptographyClient(key, credential)
+
+        signature = base64.b64decode(b64signature)
+        digest = base64.b64decode(b64hash_value)
+
+        result = crypto_client.verify(SignatureAlgorithm.es256, digest, signature)
+
+        logging.info ("Verification Result: "+str(result.is_valid))
+
+        return func.HttpResponse(
+            body=json.dumps({'message':'Signature Verification Result', 'is_valid': True}),
+            mimetype="application/json",
+            status_code=200
+        )
+
+    except Exception as e:
+        logging.error(f"An error occurred: {e}")
+        return func.HttpResponse(
+            body=json.dumps({'message': 'An error occurred during signature verification', 'error': str(e)}),
+            mimetype="application/json",
+            status_code=500
+        )
